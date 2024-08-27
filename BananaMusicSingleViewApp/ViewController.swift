9ea6d48cdc62c0501228a87c7386500086c4f8e1
Terry-Tai <tterry0626tw@gmail.com>
2024-08-27 14:46:31 +0800

    Initial Commit

---
+++ BananaMusicSingleViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BananaMusicSingleViewApp
+//
+//  Created by 戴光晨 on 2024/8/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
